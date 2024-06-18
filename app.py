bdf3664b1e6d63a5badba08e8a1d40bcdaee026c
Partha Pratim Sarmah <partha.pratim.sarmh@gmail.com>
2024-06-18 22:31:01 +0000

    spam

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def home():
 
 @app.route("/api/predict", methods=["POST"])
 def predict():
-    email_text = request.form.get('content')
-    prediction =  make_prediction(email_text)
-    return render_template("index.html", prediction=prediction, email_text=email_text)
+    email = request.form.get('content')
+    prediction =  make_prediction(email)
+    return render_template("index.html", prediction=prediction, email=email)
 
 @app.route('/api/predict', methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)  # Get data posted as a json
-    email_text = data['content']
-    prediction =  make_prediction(email_text)
-    return jsonify({'prediction': prediction, 'email_text': email_text}) 
+    email = data['content']
+    prediction =  make_prediction(email)
+    return jsonify({'prediction': prediction, 'email': email}) 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
