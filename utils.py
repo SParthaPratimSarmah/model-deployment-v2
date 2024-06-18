04367d0ee0df5ad11b9c5f4f4c7b9129177743f6
Partha Pratim Sarmah <partha.pratim.sarmh@gmail.com>
2024-06-18 22:38:22 +0000

    Finish

--- utils.py
+++ utils.py
@@ -2,8 +2,11 @@ import pickle
 cv = pickle.load(open("models/cv.pkl","rb"))
 clf = pickle.load(open("models/clf.pkl","rb"))
 
-def make_prediction(content):
-    tokenized_email = cv.transform([content]) # X 
+def make_prediction(email):
+    if email == "":
+        return ""
+    tokenized_email = cv.transform([email]) # X 
     prediction = clf.predict(tokenized_email)
+    # If the email is spam prediction should be 1
     prediction = 1 if prediction == 1 else -1
     return prediction
